f753ac9453dd2ca83185058f92ea9697c2cca265
sumin10 <umsumin10@gmail.com>
2023-01-12 19:51:41 +0900

    Feat: ProductDetail Components

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+import Button from "./../components/Button";
 
 export default function ProductDetail() {
-  return <div>ProductDetail</div>;
+  const {
+    state: {
+      product: { image, title, category, price, description, option },
+    },
+  } = useLocation();
+  const [select, setSelect] = useState(option[0]);
+  const handleSelect = (e) => setSelect(e.target.value);
+
+  return (
+    <section>
+      <img src={image} alt={title} />
+      <div>
+        <p>{category}</p>
+        <h3>{title}</h3>
+        <p>{price}원</p>
+        <p>{description}</p>
+        <p>옵션:</p>
+        <select onChange={handleSelect} value={select}>
+          {option &&
+            option.map((option, id) => <option key={id}>{option}</option>)}
+        </select>
+        <Button text="장바구니 추가" />
+      </div>
+    </section>
+  );
 }
