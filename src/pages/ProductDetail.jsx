73d6fdf3da610e1e00cc95094ffc31d020756723
sumin10 <umsumin10@gmail.com>
2023-01-13 12:27:11 +0900

    Style: ProductDetail Components

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -12,18 +12,24 @@ export default function ProductDetail() {
   const handleSelect = (e) => setSelect(e.target.value);
 
   return (
-    <section>
-      <img src={image} alt={title} />
-      <div>
-        <p>{category}</p>
-        <h3>{title}</h3>
-        <p>{price}원</p>
-        <p>{description}</p>
-        <p>옵션:</p>
-        <select onChange={handleSelect} value={select}>
-          {option &&
-            option.map((option, id) => <option key={id}>{option}</option>)}
-        </select>
+    <section className="flex flex-col md:flex-row p-4 ">
+      <img className="w-full px-4 basis-7/12" src={image} alt={title} />
+      <div className="w-full p-4 basis-5/12 flex flex-col ">
+        <p className="text-gray-500">{category}</p>
+        <h3 className="text-2xl font-semibold pb-2">{title}</h3>
+        <p className="text-xl font-semibold pb-2">{price}원</p>
+        <p className="text-gray-600 mb-10">{description}</p>
+        <div className="flex items-center">
+          <p className="text-brand font-bold">옵션:</p>
+          <select
+            className="p-2 m-4 px-10 border-2 border-brand outline-none"
+            onChange={handleSelect}
+            value={select}
+          >
+            {option &&
+              option.map((option, id) => <option key={id}>{option}</option>)}
+          </select>
+        </div>
         <Button text="장바구니 추가" />
       </div>
     </section>
