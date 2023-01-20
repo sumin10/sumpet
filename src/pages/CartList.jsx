f2ecbb789d06e335a631b40c8d0d1b51bfdaa68e
sumin10 <umsumin10@gmail.com>
2023-01-20 12:19:22 +0900

    Style: CartList Page

--- src/pages/CartList.jsx
+++ src/pages/CartList.jsx
@@ -23,30 +23,32 @@ export default function CartList() {
   const handleClick = () => navigate(`/products`);
 
   return (
-    <section>
-      <p>장바구니</p>
+    <section className="p-8">
+      <h3 className="text-2xl text-center font-bold">장바구니</h3>
       {!hasProducts ? (
-        <>
-          <h3>🐶 장바구니에 담긴 상품이 없어요.</h3>
-          <p>원하는 상품을 담아보세요!</p>
+        <div className="text-center">
+          <p className="pt-4 pb-1 text-xl">🐶 장바구니에 담긴 상품이 없어요.</p>
+          <p className="pb-4">원하는 상품을 담아보세요!</p>
           <Button text="상품 보러 가기" onClick={handleClick} />
-        </>
+        </div>
       ) : (
         <>
-          <ul>
+          <ul className="p-4 px-8">
             {products &&
               products.map((product) => (
                 <CartItem key={product.id} product={product} uid={uid} />
               ))}
           </ul>
-          <div>
+          <div className="flex justify-between items-center my-6 px-2 md:px-8 lg:px-16">
             <PriceCard text="총 상품 금액" price={totalPrice} />
-            <FiPlus />
+            <FiPlus className="shrink-0" />
             <PriceCard text="배송비" price={3000} />
-            <TbEqual />
+            <TbEqual className="shrink-0" />
             <PriceCard text="총 금액" price={totalPrice + 3000} />
+            <button className="bg-brand text-white md:text-lg py-12 px-7 rounded-xl hover:brightness-105">
+              주문하기
+            </button>
           </div>
-          <Button text="주문하기" />
         </>
       )}
     </section>
