04ec19c07908fe1fba9cb82d87a44828dc89c335
sumin10 <umsumin10@gmail.com>
2023-01-26 13:34:03 +0900

    Fix: Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,14 @@
 import React from "react";
+import ProductList from "./ProductList";
+import Banner from "./../components/Banner";
+import Footer from "./../components/Footer";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Banner />
+      <ProductList />
+      <Footer />
+    </div>
+  );
 }
